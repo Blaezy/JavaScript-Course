55336ea6296f9ffedbcf6dcc46aa713a99d73421
Blaezy <as8378129@gmail.com>
2025-12-02 03:14:52 +0530

    This is the final lesson-11 and Exercise-11 completed

---
+++ Exercises/lesson-11/11x/11x.js
@@ -0,0 +1,168 @@
+console.log("everything fine!");
+
+// let TodoList = [
+//   { taskName: "dinner tonight", taskDate: "2025-12-01", isBox: false },
+// // ];
+
+const TodoListStoredValue = JSON.parse(localStorage.getItem("TodoStorageKey"));
+console.log(TodoListStoredValue);
+let TodoList = TodoListStoredValue || [];
+
+if (TodoList === null) TodoList = [];
+
+// display the task in byDefault when refresh
+defaultDate();
+addTodoList();
+allInOne();
+
+// task display 
+function addTodoList() {
+  let desInput = document.querySelector(".description");
+  let deleteAllElement = document.querySelector(".delete-all-button");
+
+  if (TodoList.length === 0) {
+    desInput.innerHTML = `Type something in 'Todo input' and press enter or click Add`;
+    deleteAllElement.classList.add("delete-all-button-opacity");
+  } else {
+    desInput.innerHTML = ``;
+    deleteAllElement.classList.remove("delete-all-button-opacity");
+  }
+
+  let todoListElement = document.querySelector(".js-todolist");
+  todoListElement.innerHTML = "";
+
+  if (TodoList) {
+    for (let i = 0; i < TodoList.length; i++) {
+      const task = TodoList[i];
+      let taskHTML = `
+    <div class="new-task-js" >
+      <input type="checkbox" name="tasks" class="task-checkbox-js-${i} task-checkbox" onclick="updateCheckbox();allInOne();">
+      <div class="taskname-js-${i} taskname">${task.taskName}</div>
+      <div>${task.taskDate}</div>
+      <button class="delete-button" onclick="TodoList.splice(${i},1);addTodoList();allInOne();">Delete</button>
+    </div>
+    `;
+      todoListElement.innerHTML += taskHTML;
+    }
+  }
+}
+
+// getting the input value from input text
+
+function getInputbutton() {
+  const inputTodo = document.querySelector(".todo-input-js");
+  const inputDate = document.querySelector(".date-input-js");
+  const taskName = inputTodo.value;
+  const taskDate = inputDate.value;
+
+  if (taskName != "") {
+    TodoList.push({ taskName, taskDate, isBox: false });
+    inputTodo.value = "";
+  } else {
+    alert("Todo name can't be empty");
+  }
+
+  addTodoList();
+  allInOne();
+}
+
+// updating the isbox
+
+function updateCheckbox() {
+  for (let i = 0; i < TodoList.length; i++) {
+    let newClassName = `.task-checkbox-js-${i}`;
+    const box = document.querySelector(newClassName);
+
+    let taskObject = TodoList[i];
+    taskObject.isBox = box.checked;
+
+    // console.log(TodoList[i]);
+  }
+}
+
+// updating the isbox display
+function updateCheckboxDisplay() {
+  for (let i = 0; i < TodoList.length; i++) {
+    let newClassName = `.task-checkbox-js-${i}`;
+    const box = document.querySelector(newClassName);
+
+    let taskObject = TodoList[i];
+    box.checked = taskObject.isBox;
+  }
+}
+// creating the line through effect
+function makeMiddleLine() {
+  for (let i = 0; i < TodoList.length; i++) {
+    let newClassName = `.taskname-js-${i}`;
+    const inputElement = document.querySelector(newClassName);
+
+    let taskObject = TodoList[i];
+
+    if (taskObject.isBox) {
+      inputElement.classList.add("line-through-para");
+    } else {
+      inputElement.classList.remove("line-through-para");
+    }
+  }
+}
+
+function printConsole() {
+  console.log(TodoList);
+}
+
+// calculating the percentage of checkbox
+function calculatePercentage() {
+  let Total_task = TodoList.length;
+  let current_task = 0;
+  for (let i = 0; i < TodoList.length; i++) {
+    let taskObject = TodoList[i];
+    if (taskObject.isBox) {
+      current_task++;
+    }
+  }
+
+  let result_display;
+  if (Total_task > 0) {
+    const result = (current_task / Total_task) * 100;
+    result_display = `${Math.floor(result)}%`;
+  } else {
+    result_display = `%`;
+  }
+
+  const buttonElement = document.querySelector(".result-button-js");
+  buttonElement.innerHTML = result_display;
+}
+
+// Delete all TodoList and stored data
+function deletingAll() {
+  TodoList = [];
+  addTodoList();
+  calculatePercentage();
+  localStorage.removeItem("TodoStorageKey");
+}
+
+function allInOne() {
+  storeLocalData();
+  updateCheckboxDisplay();
+  makeMiddleLine();
+  calculatePercentage();
+}
+
+// input Todos from "Enter"
+function inputKeyDown(event) {
+  if (event.key === "Enter") {
+    getInputbutton();
+  }
+}
+
+//  changing the Date to default today date
+function defaultDate() {
+  const today = new Date().toISOString().split("T")[0];
+  document.querySelector(".date-input-js").value = today;
+}
+
+// TODO: Add Local save in the page
+function storeLocalData() {
+  const strTodoList = JSON.stringify(TodoList);
+  localStorage.setItem("TodoStorageKey", strTodoList);
+}
