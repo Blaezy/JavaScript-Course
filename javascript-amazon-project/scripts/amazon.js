c6e1bd5ec581d0ac3223512bd33e4a492b91e6d3
Blaezy <as8378129@gmail.com>
2025-12-16 10:04:21 +0530

    added product  in a new script

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,36 +1,5 @@
 console.log("hello Idiot");
 
-// here all the product data list 
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      star: 4.5,
-      count: 87,
-    },
-    priceInCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      star: 4,
-      count: 127,
-    },
-    priceInCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      star: 4.5,
-      count: 56,
-    },
-    priceInCents: 799,
-  },
-];
-
 let allProduct = '';
 
 products.forEach((product) => {
@@ -47,14 +16,14 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.star * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${(product.priceInCents / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
