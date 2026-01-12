55fa88142aea6205314c9b15b5ef6bc2f3f41174
Blaezy <as8378129@gmail.com>
2026-01-13 02:27:43 +0530

    some little changes were made

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 // import "../data/cart-class.js";
-import '../data/car.js';
+// import '../data/car.js';
 
 renderOderSummary();
 renderPaymentSummary();
